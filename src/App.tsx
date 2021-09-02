950fc6f8285a3b2cefa33b0c8b48290f5e5ced2b
Santiago Gonzalez <santiago.gonzalez@clarite.pl>
2021-09-02 14:11:13 +0200

    Add logo and update App.txs

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import { useRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
 import authService from "services/auth.service";
 import Login from "features/auth/Login";
 import PostFeed from "features/postsFeed/PostsFeed";
 import MyAccount from "features/myAccount/MyAccount";
-import { Link, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { ProtectedRoute } from "common/ProtectedRoute";
 import SignUp from "features/auth/SignUp";
 import NewPost from "features/editor/NewPost";
 import { useEffect } from "react";
 import { currentUserAtom } from "features/auth/auth.state";
+import { Navbar } from "components/Navbar/Navbar";
 
 function App() {
-  const [user, setUser] = useRecoilState(currentUserAtom);
+  const setUser = useSetRecoilState(currentUserAtom);
 
   useEffect(() => {
     getUserData();
@@ -29,26 +30,9 @@ function App() {
     }
   };
 
-  const logout = async () => {
-    try {
-      await setUser(null);
-      await authService.logout();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
     <>
-      <nav>
-        {user && (
-          <>
-            <button onClick={logout}>Logout</button>
-            <Link to="/">Posts feed</Link>
-            <Link to="/my-account">My Account</Link>
-          </>
-        )}
-      </nav>
+      <Navbar />
       <Switch>
         <ProtectedRoute path="/" exact>
           <PostFeed />
